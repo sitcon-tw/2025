6a0309ac8a74dd1c68702037d43d833450f6771f
naruko-hstk <34113239+naruko-hstk@users.noreply.github.com>
2024-10-25 15:47:20 +0800

    Add dynamic title support

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,9 @@
 import Image from "next/image"
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+  title: "Home．SITCON 2025",
+}
 
 export default function Home() {
   return (
