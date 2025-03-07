04afdf4d0d198ddefe99153334d601d4a570d621
Dada878 <xx0932399@gmail.com>
2025-03-08 04:42:23 +0800

    feat: add detail information dialog for all posters

--- app/(website)/(pages)/poster/page.tsx
+++ app/(website)/(pages)/poster/page.tsx
@@ -1,10 +1,31 @@
 "use client";
 
+import { useState } from "react";
 import Image from "next/image";
+import { AnimatePresence, motion } from "framer-motion";
 import posterData from "./posterData";
 import { cn } from "@/lib/utils";
 
+interface Poster {
+  title: string;
+  author: string;
+  image: string;
+  description: string;
+}
+
 export default function PosterPage() {
+  const [selectedPoster, setSelectedPoster] = useState<Poster | null>(null);
+
+  const handlePosterClick = (poster: Poster) => {
+    // TODO: uncomment this line to enable dialog
+    // setSelectedPoster(poster);
+    console.log(poster.title);
+  };
+
+  const handleCloseDialog = () => {
+    setSelectedPoster(null);
+  };
+
   return (
     <div>
       <h1 className="pb-16 text-h1 font-bold text-[#FFFFFF]">靜態海報展</h1>
@@ -19,10 +40,11 @@ export default function PosterPage() {
         今年的靜態海報展涵蓋了以下精彩內容：
       </p>
       <div className="grid grid-cols-1 justify-center gap-4 gap-y-12 md:grid-cols-2 lg:grid-cols-3">
-        {posterData.map((poster) => (
+        {posterData.map((poster: Poster) => (
           <div
             key={poster.title}
             className="cursor-pointer overflow-hidden rounded-md transition-all duration-300 hover:scale-105 hover:rounded-xl"
+            onClick={() => handlePosterClick(poster)}
           >
             <div className="group relative flex justify-center">
               <Image
@@ -45,13 +67,6 @@ export default function PosterPage() {
               {poster.title}
             </h3>
             <div className="flex items-center space-x-2 pt-2">
-              {/* <Image
-                // src={`/poster/${poster.avatars[0]}`}
-                alt={poster.author}
-                width={32}
-                height={32}
-                className="rounded-full"
-              /> */}
               <p className="text-zinc-300">{poster.author}</p>
             </div>
           </div>
@@ -60,6 +75,30 @@ export default function PosterPage() {
       <p className="mt-6">
         此外，在下午的點心時段，各海報的作者親臨現場，與大家交流討論。這是個絕佳的機會來提問、學習，並認識來自各地的厲害夥伴們，熱愛探索新知的你，千萬別錯過！
       </p>
+      <AnimatePresence>
+        {selectedPoster && (
+          <div className="fixed inset-0 z-50 flex items-center justify-center">
+            <div
+              className="absolute inset-0 bg-black bg-opacity-50"
+              onClick={handleCloseDialog}
+            />
+
+            <motion.div
+              initial={{ opacity: 0, scale: 0.8 }}
+              animate={{ opacity: 1, scale: 1 }}
+              exit={{ opacity: 0, scale: 0.8 }}
+              className="relative z-10 max-h-[50vh] w-full max-w-lg overflow-x-hidden overflow-y-scroll whitespace-pre-wrap rounded-lg bg-background p-12"
+            >
+              <h2 className="mb-4 text-2xl font-bold">
+                {selectedPoster.title}
+              </h2>
+              <p className="mb-4 whitespace-pre-line">
+                {selectedPoster.description}
+              </p>
+            </motion.div>
+          </div>
+        )}
+      </AnimatePresence>
     </div>
   );
 }
