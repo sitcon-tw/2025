4aebb6539cfc7ab46e535fad828e245d08053808
Dada878 <xx0932399@gmail.com>
2025-03-08 04:26:45 +0800

    feat: add poster data

--- app/(website)/(pages)/poster/page.tsx
+++ app/(website)/(pages)/poster/page.tsx
@@ -2,40 +2,64 @@
 
 import Image from "next/image";
 import posterData from "./posterData";
+import { cn } from "@/lib/utils";
 
 export default function PosterPage() {
   return (
     <div>
       <h1 className="pb-16 text-h1 font-bold text-[#FFFFFF]">靜態海報展</h1>
+      <p className="mb-6">
+        繼去年成功的嘗試後，今年的「靜態海報展」將再次精彩登場！這是一個議程之外的特別展區，不僅提供學生展示專案或研究成果的舞台，還讓與會者能輕鬆探索多元領域的知識，開啟更多交流與啟發的契機。
+        <br />
+        <br />
+        活動將於 <strong>2025 年 3 月 8 日年會當天 在 4 樓北側攤位</strong>
+        展出，呈現來自全台優秀學生的海報作品。無論你是關注技術創新、專案分享，還是新穎的研究發現，都可以在這裡找到讓你耳目一新的主題。
+        <br />
+        <br />
+        今年的靜態海報展涵蓋了以下精彩內容：
+      </p>
       <div className="grid grid-cols-1 justify-center gap-4 gap-y-12 md:grid-cols-2 lg:grid-cols-3">
         {posterData.map((poster) => (
           <div
             key={poster.title}
-            className="cursor-pointer transition-all duration-300 hover:scale-105 hover:rounded-xl"
+            className="cursor-pointer overflow-hidden rounded-md transition-all duration-300 hover:scale-105 hover:rounded-xl"
           >
-            <Image
-              src={`/poster/${poster.image}`}
-              alt={poster.title}
-              width={400}
-              height={10}
-              className="rounded-lg"
-            />
+            <div className="group relative flex justify-center">
+              <Image
+                src={`/poster/${poster.image}`}
+                alt={poster.title}
+                width={400}
+                height={10}
+                className="rounded-xl blur-sm transition-all group-hover:opacity-40"
+              />
+              <p
+                className={cn(
+                  "absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-nowrap text-lg font-bold text-[#fff] opacity-0 transition-all group-hover:scale-150 group-hover:opacity-100",
+                )}
+              >
+                展場解鎖完整海報
+              </p>
+            </div>
+
             <h3 className="truncate pt-4 text-xl text-[#FFFFFF]">
               {poster.title}
             </h3>
             <div className="flex items-center space-x-2 pt-2">
-              <Image
-                src={`/poster/${poster.avatars[0]}`}
+              {/* <Image
+                // src={`/poster/${poster.avatars[0]}`}
                 alt={poster.author}
                 width={32}
                 height={32}
                 className="rounded-full"
-              />
+              /> */}
               <p className="text-zinc-300">{poster.author}</p>
             </div>
           </div>
         ))}
       </div>
+      <p className="mt-6">
+        此外，在下午的點心時段，各海報的作者親臨現場，與大家交流討論。這是個絕佳的機會來提問、學習，並認識來自各地的厲害夥伴們，熱愛探索新知的你，千萬別錯過！
+      </p>
     </div>
   );
 }
--- app/(website)/(pages)/poster/posterData.ts
+++ app/(website)/(pages)/poster/posterData.ts
@@ -1,56 +1,39 @@
 export default [
   {
-    title: "我預判你的預判：中學生與 AI 圍棋理論的對戰歷程",
-    author: "tudo",
+    title: "你會投籃嗎？",
+    author: "A1u",
+    authorDescription: "目前就讀成功高中二年級。",
+    description:
+      "由於我投不到三分球，我開始錄影看自己的動作有什麼問題，可是看了看還是沒有想法，所以決定來用mediapipe偵測自己的投籃，把畫面上的投籃「量化」成關節角度變成圖表，使分析變的簡單。接著再用tensorflow實現二元分類，辨識one motion(發力連貫、投的遠) 及 two motion(先跳再投、出手點高)。我瞭解到自己投籃必須傾向one motion，最後讓我能成功輕鬆的投三分球！",
+    image: "1.jpg",
+  },
+  {
+    title:
+      "Prediction of Molecular Structure Language and Melting/Boiling Point Properties",
+    author: "Wu, Tai-Cheng",
     authorDescription:
-      "我是一名 16 歲的高中生，對於軟硬軔三位一體的電子世界充滿了好奇心。曾於在 Dimwave Tech 實習，在裡面專注於學習硬體的開發。我還在 CKCSC 學習 C++ 和演算法，搭建起了程式設計基礎。我同時也對資訊安全和人工智慧感興趣，致力於創建對這個社會、對國家、對自己有價值的項目。未來目標是在科技業留下自己的印記。我目前的目標是學習資料科學和數學、量子電腦、資訊安全和深度學習。",
-    description:
-      "我的研究動機源於對電腦圍棋的憧憬，目的在於分享我從不懂 AI 到逐步開發出我自己的圍棋機器人 SigmaGo 的過程，希望能激勵同為中學生的夢想家。在研究過程中，我從《深度學習與圍棋》和 AlphaGo 論文中學習到了蒙特卡洛樹搜索法、Policy 網路和 Value 網路等關鍵概念。我用 Pytorch 訓練卷積神經網路，並從開源網站獲得大量九路圍棋棋譜以加強訓練。面對 SigmaGo 無法完全理解圍棋規則的挑戰，我引入了 Gym_Go 模組來確保合法棋步的選擇，並透過 Go Text Protocol 與 Sabaki 介面連接，測試 SigmaGo 的實際棋力。在與不同棋力的 GNUGo 對戰中，我發現 SigmaGo 的勝率隨對手棋力提升而下降，其棋力約等於 GNUGo 的第四等級。未來，我計劃引入更多高級技術如 MCTS 法、Value 網路和強化學習，以提升 SigmaGo 的能力。",
-    tags: [`DL`, `CV`, `電腦對局`],
-    file: "我預判你的預判 中學生與AI圍棋理論的對戰歷程.pdf",
-    image: "我預判你的預判 中學生與AI圍棋理論的對戰歷程.webp",
-    avatars: ["我預判你的預判 中學生與AI圍棋理論的對戰歷程_1.webp"],
+      "Wu, Tai-Cheng is a student in The affiliated senior high school of National Taiwan Normal University. TC is specialized in computer science and have publish some research related to this in science fair. Moreover, TC is the 30th club leader of Campus Network Management Club. ",
+    description: `Background:
+Predicting molecular properties such as solubility, toxicity, melting, and boiling points is crucial for fundamental science research. However, experimental measurements are often time-consuming and cost-intensive, so we use machine learning (ML) as an approach to improve prediction accuracy.
+
+Methods:
+A dataset containing over 10k compounds was used for training shallow and deep ML models. Shallow machine learning models were implemented via PyCaret and Mordred as feature extraction. For deep machine learning models, graph neural networks (GNNs), specifically CMPNN(Communicative Message Passing Neural Network) and GCN(Graph Convolutional Network), were trained, and tuned by adjusting the number of hidden layers and sizes (neurons) in each layer.
+
+Results:
+The CMPNN model outperforms the GCN and shallow ML model for boiling point prediction(best: R² = 0.76, MAE = 23.89K for b.p.; best: R² = 0.87; MAE = 23.73K for m.p.). The top molecular descriptor of the b.p. prediction is piPC1, which is related to bond order, and that of m.p. is AATS0d, which is related to σ electron Moreau-Broto autocorrelation. 
+
+Conclusions:
+The prediction of molecular properties was improved by a comprehensive research of shallow and deep learning approaches, showcasing CMPNN model can reach the highest performance in the prediction of m.p. and b.p.(R² = 0.87 in m.p.; R² = 0.76 in b.p.). In this study, we found that the deep learning model works better than shallow ML in predicting m.p.(p<0.05). This study uses SHAP analysis to successfully identify piPC1 and AATS0d as the key prediction factors of b.p. and m.p. respectively. Moreover, this approach can be applied to predict other molecular properties. To conclude, this study not only shows a highly accurate model but also identifies the key factors of m.p. and b.p.`,
+    image: "2.jpg",
   },
   {
-    title: "演算法家家酒",
-    author: "王昱智 / 王子杰 / 白雋揚",
-    authorDescription: "目前就讀於逢甲大學資訊工程學系四年級生",
+    title:
+      "創新醫療資訊展示系統：完美結合Raspberry Pi 4、電子紙及無線動態更新技術於一體",
+    author: "Amy",
+    authorDescription: `目前就讀資工系大四，專長是嵌入式系統和人工智慧，未來想往數位IC設計發展，目前正朝著這個目標努力前進。`,
     description:
-      "設計一個「遊戲式演算法學習系統」，採用可觸摸（Tangle）的 LEGO 積木來編排關卡以及體現（Embodied）操作的學習方式。此學習方式，很像兒童的「家家酒」角色扮演遊戲，玩家透過對話、想像和創造的過程獲得趣味。同樣的，玩家在此系統中，可以扮演出題老師透過 LEGO 編輯演算法關卡，也可以扮演答題學生透過操作玩具來實現演算法步驟，是一個充滿人際互動與教育意義的遊戲。",
-    tags: [`物體偵測`, `演算法學習`],
-    file: "演算法家家酒.pdf",
-    image: "演算法家家酒.webp",
-    avatars: [
-      "演算法家家酒_1.webp",
-      "演算法家家酒_2.webp",
-      "演算法家家酒_3.webp",
-    ],
-  },
-  {
-    title: "Taiwan-LLM Tutor: Large Language Models",
-    author: "Jiawei",
-    authorDescription: `我是 Jiawei，現就讀台大資工博士二年級，並於台大資料分析與決策社擔任課程長，熱愛資料、AI、數學、教育。原本從事計算數學與電腦視覺相關研究，近期跨領域至 LLM 相關應用，渴望做出一個 AI 家教幫助偏鄉孩童與弱勢族群，目前我們還在努力的路上，成果到達及格的邊緣。
-  
-  Linkedln: [https://www.linkedin.com/in/jwliao1209/](https://www.linkedin.com/in/jwliao1209/)
-  
-  GitHub: [https://github.com/jwliao1209](https://github.com/jwliao1209)`,
-    description:
-      "在本專案中，我們想建立 AI tutor 回答高中生社會科的問題。我們整理近 30 年的學測社會科考題與社會科題庫，利用 instruction tuning fine tune TAIWAN-LLM-7B 模型，並採用 LoftQ、Lion Optimizer 等方法提升模型效能，最後比較模型於社會三科中的表現。我們的專案公開至 [https://github.com/jwliao1209/TWLLM-Tutor](https://github.com/jwliao1209/TWLLM-Tutor)。",
-    tags: [`LLM`, `NLP`, `AI`],
-    file: "Taiwan-LLM Tutor Large Language Models.pdf",
-    image: "Taiwan-LLM Tutor Large Language Models.webp",
-    avatars: ["Taiwan-LLM Tutor Large Language Models_1.webp"],
-  },
-  {
-    title: "摸透語言模型的習性：LLM 會偏袒什麼樣的文章？",
-    author: "陳妍姍",
-    authorDescription: `資工系大三學生，對 NLP、影像、演算法、神經科學、經濟學、心理學都有興趣，什麼都學一點，但什麼都沒有很厲害，還在努力探索未來的方向當中。目前在奧義智慧科技當 ML research intern。`,
-    description:
-      "隨著 ChatGPT 等語言模型的使用快速成長，可以預期未來網路上將充斥著大型自然語言模型（Large Language Models，簡稱 LLM）直接生成的文章。我們不禁好奇，這些 LLM 會不會偏好自己生成的文章呢？更明確地說，給定 LLM 數篇可用以回答問題的參考文章時，它們是否更傾向以自己生成的內容作為回答依據，造成 LLM 的回答不夠中立，甚至被自己的文章誤導、產生幻覺呢？在這場演講中，將以嚴謹的實驗討論 ChatGPT 與 Llama 2 這兩個 LLM 對「人類撰寫的文章」、「自己生成的文章」、「其他 LLM 生成的文章」分別的偏好程度。",
-    tags: [`NLP`, `LLM`, `Retrieval Augmented Generation`],
-    file: "摸透語言模型的習性 LLM 會偏袒什麼樣的文章.pdf",
-    image: "摸透語言模型的習性 LLM 會偏袒什麼樣的文章.webp",
-    avatars: ["摸透語言模型的習性 LLM 會偏袒什麼樣的文章_1.webp"],
+      "隨著智慧醫療的發展，設計高效、低功耗且易管理的資訊展示系統成為醫院管理的重要挑戰。本研究提出一套結合Raspberry Pi 4、電子紙顯示技術與無線動態更新技術的創新醫療資訊展示系統，實現智慧化管理的同時，具備顯著的醫療應用價值。系統架構包含電子紙顯示器、Raspberry Pi 4以及基於網頁的管理平台，醫護人員可以透過網頁更新床頭卡與手術狀態，後端使用Python與Flask框架，顯示內容以Pillow函式庫生成與渲染，確保穩定性與可靠性。測試結果顯示，該系統能準確更新與展示醫療資訊，並有效減少紙張與能源消耗，符合ESG永續發展目標，特別是電子紙的低功耗特性，不僅實現醫療資訊管理智慧化，更降低對環境的影響，展現強大的應用潛力。此系統在病房資訊管理等場景中的穩定性與可靠性，為智慧醫療產業的推動提供了創新解決方案，並為未來醫療資訊系統的設計與發展開創了新方向。",
+    image: "3.jpg",
   },
   {
     title:
@@ -62,22 +45,15 @@ export default [
   - [1] Bo Wu, et el. STAR: A Benchmark for Situated Reasoning in Real-World Videos. In NeurIPS 2021.
   - [2] Dohwan Ko, et el. Large Language Models are Temporal and Causal Reasoners for Video Question Answering. In EMNLP 2023.
   - [3] R Zhang, et al. LLaMA-Adapter: Efficient Fine-tuning of Language Models with Zero-init Attention. arXiv:2303.16199, 2023.`,
-    tags: [`VQA`, `LLM`, `NLP`],
-    file: "Enhanced Real-World Video Question-Answering A Selective-Based Approach.pdf",
-    image:
-      "Enhanced Real-World Video Question-Answering A Selective-Based Approach.webp",
-    avatars: [
-      "Enhanced Real-World Video Question-Answering A Selective-Based Approach_1.webp",
-    ],
+    image: "5.jpg",
   },
   {
-    title: "利用 Transformer 判斷人臉健康狀況系統",
-    author: "Lily",
-    authorDescription: `目前資工系四年級，資工越學越有興趣。`,
-    description: `人臉是人類獨特的身份標誌，提供年齡、情緒和健康等資訊。人臉與健康息息相關，通過觀察臉部，如眼睛、皮膚，可以初步推斷是否有健康問題，對於病症的及時治療和改善相當重要。本研究使用 Dlib 函式庫進行人臉偵測，以及使用 Vision Transformer 模型訓練，此模型是基於多頭自注意力機制（Multi-Head Self-Attention）的圖像式深度學習模型，利用自注意力機制提升模型訓練速度，及其可擴展性，被廣泛應用於電腦視覺領域，因此本研究使用 Vision Transformer 模型，識別人臉的健康與帶有病徵的狀況。訓練模型使用的公開資料集來自 Kaggle、Roboflow，以及使用 Google 、 Microsoft Bing 等搜尋引擎蒐集圖像。此研究使用 5 種病徵及無病徵，共 6 種類別，作為圖像資料集的類別來訓練 Vision Transformer 模型，得到的 Accuracy 為 80.24%， Loss 值為 0.9988。`,
-    tags: [`DL、臉部辨識、Transformer`],
-    file: "利用 Transformer 判斷人臉健康狀況系統.pdf",
-    image: "利用 Transformer 判斷人臉健康狀況系統.webp",
-    avatars: ["利用 Transformer 判斷人臉健康狀況系統_1.webp"],
+    title: "打造專有領域的 RAG 系統 ─ 以玉山金融挑戰賽為例",
+    author: "鱸魚",
+    authorDescription: `我是鱸魚，一位擅長自然語言處理和數據科學的高中生，主要的研究領域是 LLM 和 Agent。我曾在發表過三篇 NLP 領域的論文，並在教育部的 AICUP 競賽上獲得第二、第三名的成績，在ML 領域累積了相當多的能力和經驗。目前已透過特殊選材，錄取了陽明交大的百川學士學位學程，即將修讀人工智慧─生醫核心。`,
+    description: `"儘管大型語言模型(LLM)在通用任務上表現優異，但在處理專業領域和私有數據的問答場景時，LLM 需要仰賴外部數據作為生成回答的基礎。為了讓 LLM 能有效運用外部數據進行回答，檢索增強生成(Retrieval-Augmented Generation, RAG)技術應運而生。
+
+本次議程將以講者在玉山銀行與教育部合辦的「AI CUP 2024 玉山人工智慧公開挑戰賽－RAG與LLM在金融問答的應用」中獲得第三名的參賽經驗為基礎，深入分享如何建構精準的金融領域 RAG 系統。內容涵蓋從文檔前處理、Chunking 策略優化、Text Embdeding 與 Reranking，以及 Prompt Engineering 等關鍵環節的實作細節，並剖析各個步驟對回答精準度的影響。與會者除了能掌握 RAG 的基本原理外，更能汲取實務經驗與技巧，為建置專屬又精確的中文 RAG 系統奠定基礎。"`,
+    image: "4.jpg",
   },
 ];
