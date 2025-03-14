73a1ebc3e43d518ce33b4e751f806bae344807f4
Dada878 <xx0932399@gmail.com>
2025-03-14 18:03:59 +0800

    feat: update poster page to show all posters

--- app/(website)/(pages)/poster/page.tsx
+++ app/(website)/(pages)/poster/page.tsx
@@ -17,9 +17,7 @@ export default function PosterPage() {
   const [selectedPoster, setSelectedPoster] = useState<Poster | null>(null);
 
   const handlePosterClick = (poster: Poster) => {
-    // TODO: uncomment this line to enable dialog
-    // setSelectedPoster(poster);
-    console.log(poster.title);
+    setSelectedPoster(poster);
   };
 
   const handleCloseDialog = () => {
@@ -30,14 +28,10 @@ export default function PosterPage() {
     <div>
       <h1 className="pb-16 text-h1 font-bold text-[#FFFFFF]">靜態海報展</h1>
       <p className="mb-6">
-        繼去年成功的嘗試後，今年的「靜態海報展」將再次精彩登場！這是一個議程之外的特別展區，不僅提供學生展示專案或研究成果的舞台，還讓與會者能輕鬆探索多元領域的知識，開啟更多交流與啟發的契機。
+        今年的「靜態海報展」已在年會當天圓滿結束！感謝所有投稿者和與會者的熱情參與，我們一同見證了來自全台學生的創新與努力，透過靜態海報交流了無數精彩的想法。
         <br />
         <br />
-        活動將於 <strong>2025 年 3 月 8 日年會當天 在 4 樓北側攤位</strong>
-        展出，呈現來自全台優秀學生的海報作品。無論你是關注技術創新、專案分享，還是新穎的研究發現，都可以在這裡找到讓你耳目一新的主題。
-        <br />
-        <br />
-        今年的靜態海報展涵蓋了以下精彩內容：
+        若你錯過了當天的展覽，或是想再次回味其中的亮點，不用擔心！我們已將所有的海報作品彙整供大家瀏覽！
       </p>
       <div className="grid grid-cols-1 justify-center gap-4 gap-y-12 md:grid-cols-2 lg:grid-cols-3">
         {posterData.map((poster: Poster) => (
@@ -52,15 +46,13 @@ export default function PosterPage() {
                 alt={poster.title}
                 width={400}
                 height={10}
-                className="rounded-xl blur-sm transition-all group-hover:opacity-40"
+                className="rounded-xl transition-all group-hover:opacity-80"
               />
               <p
                 className={cn(
                   "absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 text-nowrap text-lg font-bold text-[#fff] opacity-0 transition-all group-hover:scale-150 group-hover:opacity-100",
                 )}
-              >
-                展場解鎖完整海報
-              </p>
+              ></p>
             </div>
 
             <h3 className="truncate pt-4 text-xl text-[#FFFFFF]">
@@ -72,9 +64,6 @@ export default function PosterPage() {
           </div>
         ))}
       </div>
-      <p className="mt-6">
-        此外，在下午的點心時段，各海報的作者親臨現場，與大家交流討論。這是個絕佳的機會來提問、學習，並認識來自各地的厲害夥伴們，熱愛探索新知的你，千萬別錯過！
-      </p>
       <AnimatePresence>
         {selectedPoster && (
           <div className="fixed inset-0 z-50 flex items-center justify-center">
